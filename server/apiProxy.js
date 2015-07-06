e6a49841f6fe2eee66cb392b3912e3cd987baeb5
Kosta Korenkov <7r0ggy@gmail.com>
2015-07-06 10:34:41 +0300

    Proxy Colored Coins API through node server..
    .. to workaround Access-Control-Allow-Origin absence.

---
+++ server/apiProxy.js
@@ -0,0 +1,12 @@
+var http = require('http'),
+    httpProxy = require('http-proxy');
+
+var proxy = httpProxy.createProxyServer({
+  target:'http://testnet.api.coloredcoins.org:80'
+});
+
+proxy.listen(8000);
+
+proxy.on('proxyRes', function (proxyRes, req, res) {
+  res.setHeader('Access-Control-Allow-Origin', '*');
+});
