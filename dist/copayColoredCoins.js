c0a173fd6abd2ad853976b94c2e8a9847e10836e
Kosta Korenkov <7r0ggy@gmail.com>
2015-07-11 15:15:21 +0300

    Minor refactoring of coloredCoins service

--- dist/copayColoredCoins.js
+++ dist/copayColoredCoins.js
@@ -159,14 +159,11 @@ function ColoredCoins(configService, $http, $log, bitcore) {
   };
 
   var apiHost = (configService.getSync()['coloredCoins'] || defaultConfig)['apiHost'],
-      self = this,
-      log = $log;
+      root = {};
 
-  this.log = $log;
-
-  this.handleResponse = function (data, status, cb) {
-    log.debug('Status: ', status);
-    log.debug('Body: ', JSON.stringify(data));
+  var handleResponse = function (data, status, cb) {
+    $log.debug('Status: ', status);
+    $log.debug('Body: ', JSON.stringify(data));
 
     if (status != 200 && status != 201) {
       return cb(data);
@@ -174,30 +171,30 @@ function ColoredCoins(configService, $http, $log, bitcore) {
     return cb(null, data);
   };
 
-  this.getFrom = function (api_endpoint, param, cb) {
-    log.debug('Get from:' + api_endpoint + '/' + param);
+  var getFrom = function (api_endpoint, param, cb) {
+    $log.debug('Get from:' + api_endpoint + '/' + param);
     $http.get('http://' + apiHost + '/v2/' + api_endpoint + '/' + param)
         .success(function (data, status) {
-          return self.handleResponse(data, status, cb);
+          return handleResponse(data, status, cb);
         })
         .error(function(data, status) {
-          return self.handleResponse(data, status, cb);
+          return handleResponse(data, status, cb);
         });
   };
 
-  this.postTo = function(api_endpoint, json_data, cb) {
+  var postTo = function(api_endpoint, json_data, cb) {
     $log.debug('Post to:' + api_endpoint + ". Data: " + JSON.stringify(json_data));
     $http.post('http://' + apiHost + '/v2/' + api_endpoint, json_data)
         .success(function (data, status) {
-          return self.handleResponse(data, status, cb);
+          return handleResponse(data, status, cb);
         })
         .error(function(data, status) {
-          return self.handleResponse(data, status, cb);
+          return handleResponse(data, status, cb);
         });
   };
 
 
-  this.extractAssets = function(body) {
+  var extractAssets = function(body) {
     var assets = [];
     if (!body.utxos || body.utxos.length == 0) return assets;
 
@@ -212,77 +209,78 @@ function ColoredCoins(configService, $http, $log, bitcore) {
     return assets;
   };
 
-  this.getMetadata = function(asset, cb) {
-    this.getFrom('assetmetadata', asset.assetId + "/" + asset.utxo, function(err, body){
+  var getMetadata = function(asset, cb) {
+    getFrom('assetmetadata', asset.assetId + "/" + asset.utxo, function(err, body){
       if (err) { return cb(err); }
       return cb(null, body.metadataOfIssuence);
     });
   };
 
-  this.getAssetsByAddress = function(address, cb) {
-    this.getFrom('addressinfo', address, function(err, body) {
+  var getAssetsByAddress = function(address, cb) {
+    getFrom('addressinfo', address, function(err, body) {
       if (err) { return cb(err); }
-      return cb(null, self.extractAssets(body));
+      return cb(null, extractAssets(body));
     });
   };
 
-}
+  root.init = function() {};
 
-ColoredCoins.prototype.init = function() {};
+  root.getAssets = function(address, cb) {
+    getAssetsByAddress(address, function(err, assetsInfo) {
+      if (err) { return cb(err); }
 
-ColoredCoins.prototype.getAssets = function(address, cb) {
-  var self = this;
-  this.getAssetsByAddress(address, function(err, assetsInfo) {
-    if (err) { return cb(err); }
+      $log.debug("Assets for " + address + ": \n" + JSON.stringify(assetsInfo));
 
-    self.log.debug("Assets for " + address + ": \n" + JSON.stringify(assetsInfo));
-
-    var assets = [];
-    assetsInfo.forEach(function(asset) {
-      self.getMetadata(asset, function(err, metadata) {
-        assets.push({ address: address, asset: asset, metadata: metadata });
-        if (assetsInfo.length == assets.length) {
-          return cb(assets);
-        }
+      var assets = [];
+      assetsInfo.forEach(function(asset) {
+        getMetadata(asset, function(err, metadata) {
+          assets.push({ address: address, asset: asset, metadata: metadata });
+          if (assetsInfo.length == assets.length) {
+            return cb(assets);
+          }
+        });
       });
     });
-  });
-};
-
-ColoredCoins.prototype.transferAsset = function(asset, amount, to, txIn, numSigsRequired) {
-
-  var transfer = {
-    from: asset.address,
-    fee: 1000,
-    to: [{
-      "address": to,
-      "amount": amount,
-      "assetId": asset.asset.assetId
-    }],
-    financeOutput: {
-      value: txIn.satoshis,
-      n: txIn.vout,
-      scriptPubKey: {
-        asm: new bitcore.Script(txIn.scriptPubKey).toString(),
-        hex: txIn.scriptPubKey,
-        type: 'scripthash',
-        reqSigs: numSigsRequired
-        //addresses: []
-      }
-    },
-    financeOutputTxid: txIn.txid
   };
-  console.log(transfer);
 
-  this.postTo('sendasset', transfer, function (err, body) {
-    if (err) {
-      console.log('error: ', err);
-      return;
-    }
+  root.transferAsset = function(asset, amount, to, txIn, numSigsRequired) {
 
-    console.log(body.txHex);
-  });
-};
+    var transfer = {
+      from: asset.address,
+      fee: 1000,
+      to: [{
+        "address": to,
+        "amount": amount,
+        "assetId": asset.asset.assetId
+      }],
+      financeOutput: {
+        value: txIn.satoshis,
+        n: txIn.vout,
+        scriptPubKey: {
+          asm: new bitcore.Script(txIn.scriptPubKey).toString(),
+          hex: txIn.scriptPubKey,
+          type: 'scripthash',
+          reqSigs: numSigsRequired
+          //addresses: []
+        }
+      },
+      financeOutputTxid: txIn.txid
+    };
+    console.$log(transfer);
+
+    postTo('sendasset', transfer, function (err, body) {
+      if (err) {
+        console.$log('error: ', err);
+        return;
+      }
+
+      console.$log(body.txHex);
+    });
+  };
+
+  return root;
+}
+
 
 angular.module('copayAddon.coloredCoins').service('coloredCoins', ColoredCoins);
 
