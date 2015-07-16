41df7c7d90e203e39245e5a574204fd53a884112
Kosta Korenkov <7r0ggy@gmail.com>
2015-07-16 19:00:14 +0300

    Show transfer error

--- js/controllers/assets.js
+++ js/controllers/assets.js
@@ -1,10 +1,9 @@
 'use strict';
 
-angular.module('copayAddon.coloredCoins').controller('assetsController', function ($rootScope, $scope, $modal, $timeout, $log, coloredCoins, gettext, profileService, lodash, bitcore, externalTxSigner, UTXOList) {
+angular.module('copayAddon.coloredCoins').controller('assetsController', function ($rootScope, $scope, $modal, $controller, $timeout, $log, coloredCoins, gettext, profileService, lodash, bitcore, externalTxSigner, UTXOList) {
   var self = this;
 
   this.assets = [];
-  this.error = '';
 
   var addressToPath = {};
 
@@ -28,120 +27,129 @@ angular.module('copayAddon.coloredCoins').controller('assetsController', functio
     });
   });
 
-  var setTransferError = function(err) {
-    var fc = profileService.focusedClient;
-    $log.warn(err);
-    var errMessage =
-        fc.credentials.m > 1 ? gettext('Could not create asset transfer proposal') : gettext('Could not transfer asset');
-
-    //This are abnormal situations, but still err message will not be translated
-    //(the should) we should switch using err.code and use proper gettext messages
-    errMessage = errMessage + '. ' + (err.message ? err.message : gettext('Check you connection and try again'));
-
-    this.error = errMessage;
-
-    $timeout(function() {
-      $scope.$digest();
-    }, 1);
-  };
-
-  var handleTransferError = function(err) {
-    profileService.lockFC();
-    self.setOngoingProcess();
-    return setTransferError(err);
-  };
-
-  var submitTransfer = function(asset, transfer, form) {
-    $log.debug("Asset: " + asset);
-    $log.debug("Transfer: " + transfer);
-
-    var fc = profileService.focusedClient;
-
-    if (form.$invalid) {
-      this.error = gettext('Unable to send transaction proposal');
-      return;
-    }
-
-    if (fc.isPrivKeyEncrypted()) {
-      profileService.unlockFC(function(err) {
-        if (err) return setTransferError(err);
-        return submitTransfer(asset, transfer, form);
-      });
-      return;
-    }
-
-    self.setOngoingProcess(gettext('Selecting transaction inputs'));
-    $timeout(function() {
-      var address, amount;
-
-      address = transfer._address;
-      amount = transfer._amount;
-
-      fc.sendTxProposal({
-        toAddress: address,
-        amount: 1000,
-        message: '',
-        payProUrl: null,
-        feePerKb: 1000
-      }, function(err, txp) {
-        if (err) {
-          self.setOngoingProcess();
-          profileService.lockFC();
-          return setTransferError(err);
-        }
-
-        $log.debug(txp);
-
-        // save UTXO information from Transaction Proposal
-        lodash.each(txp.inputs, function(i) {
-          var utxo = { txid: i.txid, path: i.path, index: i.vout, value: i.satoshis,
-            publicKeys: i.publicKeys,
-            scriptPubKey: { hex: i.scriptPubKey, reqSigs: txp.requiredSignatures } };
-          UTXOList.add(i.txid, utxo);
-        });
-
-        fc.removeTxProposal(txp, function(err, txpb) {
-          if (err) { return handleTransferError(err); }
-          self.setOngoingProcess(gettext('Creating transfer transaction'));
-          coloredCoins.createTransferTx(asset, amount, address, txp.inputs[0], txp.requiredSignatures, function(err, result) {
-            if (err) { return handleTransferError(err); }
-
-            var tx = new bitcore.Transaction(result.txHex);
-            $log.debug(JSON.stringify(tx.toObject(), null, 2));
-
-            self.setOngoingProcess(gettext('Signing transaction'));
-            externalTxSigner.sign(tx, fc.credentials);
-
-            self.setOngoingProcess(gettext('Broadcasting transaction'));
-            coloredCoins.broadcastTx(tx.uncheckedSerialize(), function(err, body) {
-              if (err) { return handleTransferError(err); }
-              $rootScope.$emit('Colored/TransferSent');
-            });
-          });
-        });
-      });
-    }, 100);
-  };
-
   this.openTransferModal = function(asset) {
-    var ModalInstanceCtrl = function($scope, $modalInstance) {
+
+    var AssetTransferController = function($rootScope, $scope, $modalInstance, $timeout, $log, coloredCoins, gettext,
+                                           profileService, lodash, bitcore, externalTxSigner, UTXOList) {
+      var self = this;
       $scope.asset = asset;
+
+      $scope.error = '';
+
       $rootScope.$on('Colored/TransferSent', function() {
         $scope.cancel();
         $rootScope.$emit('NewOutgoingTx');
       });
-      $scope.setOngoingProcess = self.setOngoingProcess;
-      $scope.transferAsset = function(transfer, form) {
-        submitTransfer($scope.asset, transfer, form);
-      };
+
       $scope.cancel = function() {
         $modalInstance.dismiss('cancel');
       };
+
+      $scope.resetError = function() {
+        this.error = this.success = null;
+      };
+
+      this.setOngoingProcess = function(name) {
+        $rootScope.$emit('Addon/OngoingProcess', name);
+      };
+
+      var setTransferError = function(err) {
+        var fc = profileService.focusedClient;
+        $log.warn(err);
+        var errMessage =
+            fc.credentials.m > 1 ? gettext('Could not create asset transfer proposal') : gettext('Could not transfer asset');
+
+        //This are abnormal situations, but still err message will not be translated
+        //(the should) we should switch using err.code and use proper gettext messages
+        err.message = err.error ? err.error : err.message;
+        errMessage = errMessage + '. ' + (err.message ? err.message : gettext('Check you connection and try again'));
+
+        $scope.error = errMessage;
+
+        $timeout(function() {
+          $scope.$digest();
+        }, 1);
+      };
+
+      var handleTransferError = function(err) {
+        profileService.lockFC();
+        self.setOngoingProcess();
+        return setTransferError(err);
+      };
+
+      $scope.transferAsset = function(transfer, form) {
+        $log.debug("Asset: " + asset);
+        $log.debug("Transfer: " + transfer);
+
+        var fc = profileService.focusedClient;
+
+        if (form.$invalid) {
+          this.error = gettext('Unable to send transaction proposal');
+          return;
+        }
+
+        if (fc.isPrivKeyEncrypted()) {
+          profileService.unlockFC(function(err) {
+            if (err) return setTransferError(err);
+            return submitTransfer(asset, transfer, form);
+          });
+          return;
+        }
+
+        self.setOngoingProcess(gettext('Selecting transaction inputs'));
+        $timeout(function() {
+          var address, amount;
+
+          address = transfer._address;
+          amount = transfer._amount;
+
+          fc.sendTxProposal({
+            toAddress: address,
+            amount: 1000,
+            message: '',
+            payProUrl: null,
+            feePerKb: 1000
+          }, function(err, txp) {
+            if (err) { return handleTransferError(err); }
+
+            $log.debug(txp);
+
+            // save UTXO information from Transaction Proposal
+            lodash.each(txp.inputs, function(i) {
+              var utxo = { txid: i.txid, path: i.path, index: i.vout, value: i.satoshis,
+                publicKeys: i.publicKeys,
+                scriptPubKey: { hex: i.scriptPubKey, reqSigs: txp.requiredSignatures } };
+              UTXOList.add(i.txid, utxo);
+            });
+
+            fc.removeTxProposal(txp, function(err, txpb) {
+              if (err) { return handleTransferError(err); }
+              self.setOngoingProcess(gettext('Creating transfer transaction'));
+              coloredCoins.createTransferTx(asset, amount, address, txp.inputs[0], txp.requiredSignatures, function(err, result) {
+                if (err) { return handleTransferError(err); }
+
+                var tx = new bitcore.Transaction(result.txHex);
+                $log.debug(JSON.stringify(tx.toObject(), null, 2));
+
+                self.setOngoingProcess(gettext('Signing transaction'));
+                externalTxSigner.sign(tx, fc.credentials);
+
+                self.setOngoingProcess(gettext('Broadcasting transaction'));
+                coloredCoins.broadcastTx(tx.uncheckedSerialize(), function(err, body) {
+                  if (err) { return handleTransferError(err); }
+                  $rootScope.$emit('Colored/TransferSent');
+                });
+              });
+            });
+          });
+        }, 100);
+      };
     };
+
     var modalInstance = $modal.open({
       templateUrl: 'colored-coins/views/modals/send.html',
       windowClass: 'full animated slideInUp',
-      controller: ModalInstanceCtrl
+      controller: AssetTransferController
     });
 
     modalInstance.result.finally(function() {
