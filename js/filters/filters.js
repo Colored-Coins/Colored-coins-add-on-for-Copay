35d31eb7ba0fc2dd8c627306b69b647b21b6b2b9
Kosta Korenkov <7r0ggy@gmail.com>
2015-08-06 14:26:42 +0300

    Remove unused filter

--- js/filters/filters.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-
-angular.module('copayAddon.coloredCoins')
-  .filter('stringify', function($sce) {
-    return function(json) {
-      json = json || [];
-      return $sce.trustAsHtml(JSON.stringify(json, null, 4).replace(/\n/g, '<br>'));
-    }
-  });
