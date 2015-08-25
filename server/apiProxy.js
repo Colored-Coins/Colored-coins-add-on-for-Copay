e1ab5e4e0ab8d723c5abd482189d5724710e2dec
Kosta Korenkov <7r0ggy@gmail.com>
2015-08-25 07:28:43 +0300

    CC API Proxy: Update user-agent to include Copay string

--- server/apiProxy.js
+++ server/apiProxy.js
@@ -1,9 +1,14 @@
 var http = require('http'),
-    httpProxy = require('http-proxy');
+    httpProxy = require('http-proxy'),
+    version = require('../bower.json').version;
 
 var cors = function (proxyRes, req, res) {
   res.setHeader('Access-Control-Allow-Origin', '*');
   res.setHeader('Access-Control-Allow-Headers', 'Content-type');
+};
+
+var userAgent = function (proxyReq, req, res, options) {
+  proxyReq.setHeader('User-Agent', req.headers['user-agent'] + ' Copay/' + version);
 };
 
 
@@ -17,9 +22,11 @@ var mainnetProxy = httpProxy.createProxyServer({
 
 
 testnetProxy
+    .on('proxyReq', userAgent)
     .on('proxyRes', cors)
     .listen(8000);
 
 mainnetProxy
+    .on('proxyReq', userAgent)
     .on('proxyRes', cors)
     .listen(8100);
